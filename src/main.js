703784c035057f1887bfa1b6ac08a0e7cb5fddc9
Sinlon <1798007590@qq.com>
2020-10-01 16:59:01 +0800

    vuex

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
 import axios from 'axios'
+import store from './store'
 
 Vue.config.productionTip = false
 
@@ -11,5 +12,6 @@ axios.defaults.baseURL = 'http://106.75.154.40:9003'
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
