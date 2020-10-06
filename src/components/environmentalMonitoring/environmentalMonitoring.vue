66e144ae7dab47ce27b822012ddf225003a3f129
Sinlon <1798007590@qq.com>
2020-10-06 21:01:02 +0800

    em

--- src/components/environmentalMonitoring/environmentalMonitoring.vue
+++ src/components/environmentalMonitoring/environmentalMonitoring.vue
@@ -13,14 +13,27 @@
         <div class="lxl-left">
           <div>
             <el-table :data="tableData" style="width: 100%">
-              <el-table-column prop="date" label="日期" width="140">
+              <el-table-column prop="date" label="详情" width="140">
               </el-table-column>
-              <el-table-column prop="name" label="姓名" width="140">
+              <el-table-column prop="name" label="数据" width="140">
+              </el-table-column>
+            </el-table>
+          </div>
+          <div>
+            <el-table :data="tableData" style="width: 100%">
+              <el-table-column prop="date" label="详情" width="140">
+              </el-table-column>
+              <el-table-column prop="name" label="数据" width="140">
               </el-table-column>
             </el-table>
           </div>
         </div>
-        <div class="lxl-main">main</div>
+        <div class="lxl-main">
+          <div>
+            <div class="lxl-em"></div>
+            <div class="chart1"></div>
+          </div>
+        </div>
         <div class="lxl-right">
           <div>
             <el-alert title="成功提示的文案" type="success" :closable="false">
@@ -127,9 +140,245 @@ export default {
       },
     };
   },
+  mounted() {
+    this.chart1();
+  },
   methods: {
     handleNodeClick(data) {
       console.log(data);
+    },
+    chart1() {
+      let myChart = this.$echarts.init(document.querySelector(".chart1"));
+      let option = {
+        backgroundColor: "#f0f9eb",
+        title: {
+          text: "哎呦,不错哦",
+          textStyle: {
+            align: "center",
+            color: "#fff",
+            fontSize: 20,
+          },
+          top: "5%",
+          left: "center",
+        },
+        tooltip: {
+          trigger: "axis",
+          axisPointer: {
+            lineStyle: {
+              color: {
+                type: "linear",
+                x: 0,
+                y: 0,
+                x2: 0,
+                y2: 1,
+                colorStops: [
+                  {
+                    offset: 0,
+                    color: "rgba(0, 255, 233,0)",
+                  },
+                  {
+                    offset: 0.5,
+                    color: "rgba(255, 255, 255,1)",
+                  },
+                  {
+                    offset: 1,
+                    color: "rgba(0, 255, 233,0)",
+                  },
+                ],
+                global: false,
+              },
+            },
+          },
+        },
+        grid: {
+          top: "15%",
+          left: "5%",
+          right: "5%",
+          bottom: "15%",
+          // containLabel: true
+        },
+        xAxis: [
+          {
+            type: "category",
+            axisLine: {
+              show: true,
+            },
+            splitArea: {
+              // show: true,
+              color: "#f00",
+              lineStyle: {
+                color: "#f00",
+              },
+            },
+            axisLabel: {
+              color: "#fff",
+            },
+            splitLine: {
+              show: false,
+            },
+            boundaryGap: false,
+            data: ["A", "B", "C", "D", "E", "F"],
+          },
+        ],
+
+        yAxis: [
+          {
+            type: "value",
+            min: 0,
+            // max: 140,
+            splitNumber: 4,
+            splitLine: {
+              show: true,
+              lineStyle: {
+                color: "rgba(255,255,255,0.1)",
+              },
+            },
+            axisLine: {
+              show: false,
+            },
+            axisLabel: {
+              show: false,
+              margin: 20,
+              textStyle: {
+                color: "#d1e6eb",
+              },
+            },
+            axisTick: {
+              show: false,
+            },
+          },
+        ],
+        series: [
+          {
+            name: "注册总量",
+            type: "line",
+            // smooth: true, //是否平滑
+            showAllSymbol: true,
+            // symbol: 'image://./static/images/guang-circle.png',
+            symbol: "circle",
+            symbolSize: 25,
+            lineStyle: {
+              normal: {
+                color: "#6c50f3",
+                shadowColor: "rgba(0, 0, 0, .3)",
+                shadowBlur: 0,
+                shadowOffsetY: 5,
+                shadowOffsetX: 5,
+              },
+            },
+            label: {
+              show: true,
+              position: "top",
+              textStyle: {
+                color: "#6c50f3",
+              },
+            },
+            itemStyle: {
+              color: "#6c50f3",
+              borderColor: "#fff",
+              borderWidth: 3,
+              shadowColor: "rgba(0, 0, 0, .3)",
+              shadowBlur: 0,
+              shadowOffsetY: 2,
+              shadowOffsetX: 2,
+            },
+            tooltip: {
+              show: false,
+            },
+            areaStyle: {
+              normal: {
+                color: new this.$echarts.graphic.LinearGradient(
+                  0,
+                  0,
+                  0,
+                  1,
+                  [
+                    {
+                      offset: 0,
+                      color: "rgba(108,80,243,0.3)",
+                    },
+                    {
+                      offset: 1,
+                      color: "rgba(108,80,243,0)",
+                    },
+                  ],
+                  false
+                ),
+                shadowColor: "rgba(108,80,243, 0.9)",
+                shadowBlur: 20,
+              },
+            },
+            data: [502.84, 205.97, 332.79, 281.55, 398.35, 214.02],
+          },
+          {
+            name: "注册总量",
+            type: "line",
+            // smooth: true, //是否平滑
+            showAllSymbol: true,
+            // symbol: 'image://./static/images/guang-circle.png',
+            symbol: "circle",
+            symbolSize: 25,
+            lineStyle: {
+              normal: {
+                color: "#00ca95",
+                shadowColor: "rgba(0, 0, 0, .3)",
+                shadowBlur: 0,
+                shadowOffsetY: 5,
+                shadowOffsetX: 5,
+              },
+            },
+            label: {
+              show: true,
+              position: "top",
+              textStyle: {
+                color: "#00ca95",
+              },
+            },
+
+            itemStyle: {
+              color: "#00ca95",
+              borderColor: "#fff",
+              borderWidth: 3,
+              shadowColor: "rgba(0, 0, 0, .3)",
+              shadowBlur: 0,
+              shadowOffsetY: 2,
+              shadowOffsetX: 2,
+            },
+            tooltip: {
+              show: false,
+            },
+            areaStyle: {
+              normal: {
+                color: new this.$echarts.graphic.LinearGradient(
+                  0,
+                  0,
+                  0,
+                  1,
+                  [
+                    {
+                      offset: 0,
+                      color: "rgba(0,202,149,0.3)",
+                    },
+                    {
+                      offset: 1,
+                      color: "rgba(0,202,149,0)",
+                    },
+                  ],
+                  false
+                ),
+                shadowColor: "rgba(0,202,149, 0.9)",
+                shadowBlur: 20,
+              },
+            },
+            data: [281.55, 398.35, 214.02, 179.55, 289.57, 356.14],
+          },
+        ],
+      };
+      myChart.setOption(option);
+      // 自适应盒子大小,以及屏幕大小
+      window.addEventListener("resize", function () {
+        myChart.resize();
+      });
     },
   },
 };
@@ -156,14 +405,24 @@ export default {
     box-shadow: 0 2px 4px rgba(0, 0, 0, 0.12), 0 0 6px rgba(0, 0, 0, 0.04);
     display: flex;
     justify-content: center;
+    flex-direction: column;
     div {
       height: 20rem;
-      background-color: red;
+      border-bottom: solid 1px rgb(78, 196, 98);
     }
   }
   .lxl-main {
     width: 55%;
     box-shadow: 0 2px 4px rgba(0, 0, 0, 0.12), 0 0 6px rgba(0, 0, 0, 0.04);
+    div {
+      .lxl-em {
+        height: 20rem;
+        background-color:#67C23A;
+      }
+      .chart1 {
+        height: 20rem;
+      }
+    }
   }
   .lxl-right {
     width: 20%;
