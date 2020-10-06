05fac135a9f8185fd2136a70fb7f61c2e6f531ae
Sinlon <1798007590@qq.com>
2020-10-06 19:51:07 +0800

    em

---
+++ src/components/environmentalMonitoring/environmentalMonitoring.vue
@@ -0,0 +1,173 @@
+<template>
+  <div class="lxl-body">
+    <div class="lxl-box">
+      <el-breadcrumb
+        separator-class="el-icon-arrow-right"
+        class="lxl-breadcrumb"
+      >
+        <el-breadcrumb-item>当前位置</el-breadcrumb-item>
+        <el-breadcrumb-item>环境监控</el-breadcrumb-item>
+      </el-breadcrumb>
+      <el-divider></el-divider>
+      <div class="lxl-container">
+        <div class="lxl-left">
+          <div>
+            <el-table :data="tableData" style="width: 100%">
+              <el-table-column prop="date" label="日期" width="140">
+              </el-table-column>
+              <el-table-column prop="name" label="姓名" width="140">
+              </el-table-column>
+            </el-table>
+          </div>
+        </div>
+        <div class="lxl-main">main</div>
+        <div class="lxl-right">
+          <div>
+            <el-alert title="成功提示的文案" type="success" :closable="false">
+            </el-alert>
+            <el-tree
+              :data="data"
+              :props="defaultProps"
+              accordion
+              @node-click="handleNodeClick"
+            >
+            </el-tree>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      tableData: [
+        {
+          date: "2016-05-02",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1518 弄",
+        },
+        {
+          date: "2016-05-04",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1517 弄",
+        },
+        {
+          date: "2016-05-01",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1519 弄",
+        },
+        {
+          date: "2016-05-03",
+          name: "王小虎",
+          address: "上海市普陀区金沙江路 1516 弄",
+        },
+      ],
+      data: [
+        {
+          label: "一级 1",
+          children: [
+            {
+              label: "二级 1-1",
+              children: [
+                {
+                  label: "三级 1-1-1",
+                },
+              ],
+            },
+          ],
+        },
+        {
+          label: "一级 2",
+          children: [
+            {
+              label: "二级 2-1",
+              children: [
+                {
+                  label: "三级 2-1-1",
+                },
+              ],
+            },
+            {
+              label: "二级 2-2",
+              children: [
+                {
+                  label: "三级 2-2-1",
+                },
+              ],
+            },
+          ],
+        },
+        {
+          label: "一级 3",
+          children: [
+            {
+              label: "二级 3-1",
+              children: [
+                {
+                  label: "三级 3-1-1",
+                },
+              ],
+            },
+            {
+              label: "二级 3-2",
+              children: [
+                {
+                  label: "三级 3-2-1",
+                },
+              ],
+            },
+          ],
+        },
+      ],
+      defaultProps: {
+        children: "children",
+        label: "label",
+      },
+    };
+  },
+  methods: {
+    handleNodeClick(data) {
+      console.log(data);
+    },
+  },
+};
+</script>
+<style lang="less" scoped>
+.lxl-body {
+  display: flex;
+  //   flex-direction: column;
+  justify-content: center;
+  .lxl-breadcrumb {
+    margin-top: 25px;
+    margin-left: 18px;
+    margin-bottom: -10px;
+  }
+}
+.lxl-box {
+  width: 1150px;
+}
+.lxl-container {
+  display: flex;
+  flex-direction: row;
+  .lxl-left {
+    width: 25%;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.12), 0 0 6px rgba(0, 0, 0, 0.04);
+    display: flex;
+    justify-content: center;
+    div {
+      height: 20rem;
+      background-color: red;
+    }
+  }
+  .lxl-main {
+    width: 55%;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.12), 0 0 6px rgba(0, 0, 0, 0.04);
+  }
+  .lxl-right {
+    width: 20%;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.12), 0 0 6px rgba(0, 0, 0, 0.04);
+  }
+}
+</style>
