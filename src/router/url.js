e63bc32e3e22a6aa5a51b62315fa6a6dcdf71d06
哒哒哒 <1224480355@qq.com>
2020-10-18 16:41:16 +0800

    accomplish shopDetail

--- src/router/url.js
+++ src/router/url.js
@@ -4,7 +4,7 @@ module.exports = {
         Base_M1_URL: 'http://106.75.154.40:9012/traceability', //慈峰
         Base_M2_URL: 'http://120.79.63.74:9013', //晓钰
         Base_M3_URL: 'http://120.79.63.74:9013', //梓渊
-        Base_M4_URL: 'http://120.79.63.74:9013', //敏新
+        Base_M4_URL: 'http://134.175.208.235:9002', //敏新
         Base_M5_URL: 'http://120.79.63.74:9013', //宇柯
         Base_M6_URL: 'http://120.79.63.74:9013', //典明
         Base_M7_URL: 'http://120.79.63.74:9013', //模块四接口地址
