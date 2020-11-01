b6e1403ad53d11ca4d40945b1943b4e66d16d4b4
Sinlon <1798007590@qq.com>
2020-11-01 20:39:13 +0800

    lsx

--- src/components/fishingSkill/powerPoint.vue
+++ src/components/fishingSkill/powerPoint.vue
@@ -18,12 +18,6 @@
             v-model="input"
             class="input-with-select"
           >
-            <el-select v-model="select" slot="prepend" placeholder="请选择">
-              <el-option label="PPT" value="1"></el-option>
-              <el-option label="PPTX" value="2"></el-option>
-              <el-option label="PDF" value="3"></el-option>
-              <el-option label="WORD" value="4"></el-option>
-            </el-select>
             <el-button slot="append" icon="el-icon-search"></el-button>
           </el-input>
         </div>
