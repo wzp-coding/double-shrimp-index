e1bcb7221e2808e93bc8d02fdb75e07a0b397947
真好 <2713554182@qq.com>
2020-11-05 23:01:27 +0800

    没做token判断

--- src/main.js
+++ src/main.js
@@ -27,6 +27,7 @@ axios.defaults.baseM13URL = domain.Base_M13_URL
 axios.defaults.baseM2URL = domain.Base_M2_URL
 //->3,模块三接口,url地址
 axios.defaults.baseM3URL = domain.Base_M3_URL
+axios.defaults.baseM31URL = domain.Base_M31_URL
 //->4,模块四接口,url地址
 axios.defaults.baseM4URL = domain.Base_M4_URL
 axios.defaults.baseM41URL = domain.Base_M41_URL
