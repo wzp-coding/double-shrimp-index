fe6f7985798bcd9983efccd0b2b767ba45037d94
哒哒哒 <1224480355@qq.com>
2020-11-29 23:28:08 +0800

    msg

--- src/components/login/nav.vue
+++ src/components/login/nav.vue
@@ -80,7 +80,7 @@ export default {
       activeIndex: "",
       navPath: this.$route.path,
       userData1: {
-        loginId: "尚未登录",
+        loginId: "登录",
         role: "游客",
         photo:
           "https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png",
