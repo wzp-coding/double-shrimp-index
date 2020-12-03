f3332176d5db4e2caef12968af42b4e9d6c5ec19
hwpo <2236277721@qq.com>
2020-12-03 20:18:33 +0800

    。。

--- src/components/fishingSkill/powerPoint.vue
+++ src/components/fishingSkill/powerPoint.vue
@@ -17,46 +17,54 @@
             placeholder="请输入内容"
             v-model="queryInfo.query"
             clearable
-            @clear="pptList"
+            @clear="getNewTableList"
             class="input-with-select"
           >
-            <el-button slot="append" icon="el-icon-search" @click="searchPPTList"></el-button>
+            <el-button
+              slot="append"
+              icon="el-icon-search"
+              @click="handleQueryByKeysBtn"
+            ></el-button>
           </el-input>
         </div>
       </div>
       <div class="format">
         <label for="">类型： </label>
-        <el-button @click="pptList">全部</el-button>
-        <el-button v-for="item in formattab" :key="item.id" @click="formatList(item.id)">{{
-          item.name
-        }}</el-button>
-        
+        <el-button @click="handleQueryAllBtn">全部</el-button>
+        <el-button
+          v-for="item in formattab"
+          :key="item.id"
+          @click="handleQueryTypePPtBtn(item.id)"
+          >{{ item.name }}</el-button
+        >
       </div>
-      <div class="lxl-card">
+      <div class="lxl-card" v-loading="loading">
         <div class="PPT-item">
-          <!-- v-for="(item, i) in 10" :key="i" -->
           <div v-for="item in pptlist" :key="item.id">
             <el-card class="card">
-              <el-image
-                :src="item.pic"
-                class="image"
-              >
-              </el-image>
+              <el-image :src="item.pic" class="image"> </el-image>
               <div>
                 <div>
                   <p style="padding: 10px">
-                   {{item.title}}
-
+                    {{ item.title }}
                   </p>
                 </div>
                 <div class="btns">
                   <a :href="item.contentUrl" :download="item.contentUrl">
-                    <i class="el-icon-download" style="color: green" @click="pptList">下载</i></a>
-                  <i
+                    <span
+                      class="el-icon-download"
+                      style="color: green"
+                      @click="getAllpptList"
+                      >下载</span
+                    ></a
+                  >
+
+                  <span
                     class="el-icon-view"
                     style="margin-right: 10px; color: #4398d0"
                     @click="bindPreview(item.contentUrl)"
-                    >查看</i
+                  >
+                    查看</span
                   >
                 </div>
               </div>
@@ -65,18 +73,17 @@
         </div>
       </div>
       <div class="pagination">
-         <!-- 分页区域 -->
-            <el-pagination
-              @size-change="handleSizeChange"
-              @current-change="handleCurrentChange"
-              :current-page="queryInfo.pagenum"
-              :page-size="queryInfo.pagesize"
-              layout="prev, pager,next, total, jumper"
-              :total="total"
-              style="padding-left: 200px;
-              padding-top: 10px;"
-              >
-            </el-pagination>
+        <!-- 分页区域 -->
+        <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="queryInfo.pagenum"
+          :page-size="12"
+          layout="prev, pager,next, total, jumper"
+          :total="total"
+          style="padding-left: 200px; padding-top: 10px"
+        >
+        </el-pagination>
       </div>
     </div>
   </div>
@@ -92,99 +99,167 @@ export default {
       pptlist: [],
       // 分页区域查询参数对象
       queryInfo: {
-        query: '',
+        query: "",
         pagenum: 1,
-        pagesize: 12,
       },
       // 总数据条数
       total: 0,
-      
-    }
+      // 搜索分页
+      // state:0 表示搜索全部，1表示按类型搜索，2表示按关键字搜索
+      state: 0,
+      // 加载页面
+      loading: true,
+    };
   },
-  created () {
+  created() {
     // 所有演示文稿
-    this.pptList();
-    // 搜索演示文稿
-    this.searchPPTList();
-    this.formatTab();
-    this.formatList();
-
+    this.getAllpptList();
+    // 分类
+    this.getPPtTypes();
   },
   methods: {
-    async pptList() {
-        const { data: res } = await this.reqM2Service("/education/manuscripts/findAll/"+ this.queryInfo.pagenum+"/"+this.queryInfo.pagesize,{
-        params: this.queryInfo
-      },'get')
-      console.log(res)
+    // 点击 全部 按钮
+    handleQueryAllBtn() {
+      this.state = 0;
+      this.queryInfo.pagenum = 1;
+      this.getAllpptList();
+    },
+    // 点击 各种类型 按钮
+    handleQueryTypePPtBtn(id) {
+      this.state = 1;
+      this.queryInfo.pagenum = 1;
+      this.getPPTbyTypeId(id);
+    },
+    // 点击 搜索 按钮
+    handleQueryByKeysBtn() {
+      this.state = 2;
+      this.queryInfo.pagenum = 1;
+      this.searchPPTList();
+    },
+    // 全部演示文稿
+    async getAllpptList() {
+      const { data: res } = await this.reqM2Service(
+        "/education/manuscripts/findAll/" + this.queryInfo.pagenum + "/8",
+        {
+          params: this.queryInfo,
+        },
+        "get"
+      );
+      if (res.code !== 20000) {
+        return this.$message.error("获取列表失败！");
+      }
+      this.pptlist = res.data.rows;
+      this.total = res.data.total;
+      this.loading = false;
+      console.log(this.pptlist);
+    },
 
-        if (res.code !== 20000) {
-        return this.$message.error('获取列表失败！')
+    // 刷新演示文稿
+    async getNewTableList() {
+      const { data: res } = await this.reqM2Service(
+        "/education/manuscripts/findAll/1/8",
+        {
+          params: this.queryInfo,
+        },
+        "get"
+      );
+      if (res.code !== 20000) {
+        return this.$message.error("获取列表失败！");
       }
-        this.pptlist = res.data.rows
-        this.total = res.data.total
-        console.log(this.pptlist)
-      },
-      // 分页区域
-      handleSizeChange(newSize) {
-        this.queryInfo.pagesize = newSize
-        this.pptList()
-      },
-      handleCurrentChange(newPage) {
-        this.queryInfo.pagenum = newPage
-        this.pptList()
-      },
-      // 搜索演示文稿
-      async searchPPTList() {
-        const { data: res } = await this.reqM13Service("/education/search/time/"+ this.queryInfo.pagenum+"/"+this.queryInfo.pagesize+"/2?key="+this.queryInfo.query,{
-        params: this.queryInfo
-      },'get')
-        console.log(res)
-        if (res.code !== 20000) {
-        return this.$message.error('获取列表失败！')
+      this.pptlist = res.data.rows;
+      this.total = res.data.total;
+      this.queryInfo.pagenum = 1;
+      console.log(this.pptlist);
+    },
+    // 分页区域
+    handleSizeChange(newSize) {
+      this.queryInfo.pagesize = newSize;
+      if (this.state == 0) {
+        this.getAllpptList();
+      } else if (this.state == 1) {
+        this.getPPTbyTypeId(id);
+      } else if (this.state == 2) {
+        this.searchPPTList();
       }
-        this.pptlist = res.data.rows
-        this.total = res.data.total
-        console.log(this.pptlist)
-      },
-      // 查找演示文稿类型
-      async formatTab() {
-        const { data: res } = await this.reqM2Service("/education/manuscriptsTypes" , "" ,'get')
-        console.log(res)
-        if (res.code !== 20000) {
-        return this.$message.error('获取列表失败！')
+    },
+    handleCurrentChange(newPage) {
+      this.queryInfo.pagenum = newPage;
+      if (this.state == 0) {
+        this.getAllpptList();
+      } else if (this.state == 1) {
+        this.getPPTbyTypeId(id);
+      } else if (this.state == 2) {
+        this.searchPPTList();
       }
-        this.formattab = res.data;
-        console.log(this.formattab);
-        
-      },
-      // 按类型查找
-      async formatList(id) {
-        const { data: res } = await this.reqM2Service("/education/manuscripts/search/searchByTypeId/" + id +"/"+ this.queryInfo.pagenum+"/"+this.queryInfo.pagesize,
-         "" , 'get')
-        console.log(res)
-        if (res.code !== 20000) {
-        return this.$message.error('获取列表失败！')
+    },
+    // 搜索演示文稿
+    async searchPPTList() {
+      const { data: res } = await this.reqM13Service(
+        "/education/search/time/" +
+          this.queryInfo.pagenum +
+          "/8/2?key=" +
+          this.queryInfo.query,
+        {
+          params: this.queryInfo,
+        },
+        "get"
+      );
+      console.log(res);
+      if (res.code !== 20000) {
+        return this.$message.error("获取列表失败！");
       }
-        this.pptlist = res.data.rows
-        this.total = res.data.total
-        console.log(this.pptlist)
-      },
-      
-      bindPreview(url) {
-        console.log(url);
-        // console.log(/(doc)|(ppt)|(pptx)|(xls)/.test(url));
-        // 跳转到空白页面预览，得等域名部署后
-        if (/(doc)|(ppt)|(pptx)|(xls)/.test(url)) {
-          url =
-            "http://view.officeapps.live.com/op/view.aspx?src=" + encodeURIComponent(url);
-           // 测试
-          url =
-          "https://view.officeapps.live.com/op/view.aspx?src=http%3a%2f%2fvideo.ch9.ms%2fbuild%2f2011%2fslides%2fTOOL-532T_Sutter.pptx";
-        }
-        // console.log(url);
-        window.open(url);
-    }
-  }
+      this.pptlist = res.data.rows;
+      this.total = res.data.total;
+      console.log(this.pptlist);
+    },
+    // 获取演示文稿的类型
+    async getPPtTypes() {
+      const { data: res } = await this.reqM2Service(
+        "/education/manuscriptsTypes",
+        "",
+        "get"
+      );
+      console.log(res);
+      if (res.code !== 20000) {
+        return this.$message.error("获取列表失败！");
+      }
+      this.formattab = res.data;
+      console.log(this.formattab);
+    },
+    // 按类型查找演示文稿
+    async getPPTbyTypeId(id) {
+      const { data: res } = await this.reqM2Service(
+        "/education/manuscripts/search/searchByTypeId/" +
+          id +
+          "/" +
+          this.queryInfo.pagenum +
+          "/8",
+        { params: this.queryInfo },
+        "get"
+      );
+      if (res.code !== 20000) {
+        return this.$message.error("获取列表失败！");
+      }
+      this.pptlist = res.data.rows;
+      this.total = res.data.total;
+      console.log(this.pptlist);
+    },
+
+    bindPreview(url) {
+      console.log(url); // console.log(/(doc)|(ppt)|(pptx)|(xls)/.test(url)); // 跳转到空白页面预览，得等域名部署后
+      if (/(doc)|(ppt)|(pptx)|(xls)/.test(url)) {
+        url =
+          "http://view.officeapps.live.com/op/view.aspx?src=" +
+          encodeURIComponent(url);
+      } 
+      // console.log(url);
+      //#region 包裹起来的区域是测试的
+      url =
+        "https://view.officeapps.live.com/op/view.aspx?src=http%3a%2f%2fvideo.ch9.ms%2fbuild%2f2011%2fslides%2fTOOL-532T_Sutter.pptx";
+      //#endregion
+      window.open(url);
+    },
+  },
 };
 </script>
 <style lang="less" scoped>
@@ -197,7 +272,7 @@ export default {
   display: flex;
   flex-direction: row-reverse;
 }
-.btns:hover{
+.btns:hover {
   cursor: pointer;
 }
 .ccy-drvider {
@@ -238,6 +313,7 @@ export default {
     .card {
       margin: 5px;
       width: 275px;
+      height: 287px;
     }
   }
 }
